25c4d698f64f94dd80453e2e43ac0ff8702b5609
김태빈 <tb081212@naver.com>
2023-10-09 00:04:13 +0900

    회원관리,수정

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -3,17 +3,19 @@ package com.shop.controller;
 import com.shop.dto.MemberFormDto;
 import com.shop.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.shop.entity.Member;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import org.springframework.validation.BindingResult;
 import jakarta.validation.Valid;
+
+import java.util.List;
 
 @RequestMapping("/members")
 @Controller
@@ -57,5 +59,37 @@ public class MemberController {
         model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
         return "/member/memberLoginForm";
     }
+
+    @GetMapping("/manage")
+    public String manageMembers(Model model) {
+        List<Member> members = memberService.findAllMembers();
+        model.addAttribute("members", members);
+        return "member/manageMembers";
+    }
+
+    @GetMapping("/mypage")
+    public String myPage(Model model, @AuthenticationPrincipal User user) {
+        if (user != null) {
+            Member member = memberService.findByEmail(user.getUsername());
+            model.addAttribute("member", member);
+        }
+        return "member/myPage";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editMemberForm(@PathVariable Long id, Model model) {
+        Member member = memberService.findById(id);
+        model.addAttribute("member", member);
+        return "member/editMember";
+    }
+
+    // 회원 정보 수정 처리
+    @PostMapping("/update")
+    public String updateMember(@ModelAttribute Member member) {
+        memberService.updateMember(member);
+        return "redirect:/members/manage";
+    }
+
+
 
 }
--- src/main/java/com/shop/service/MemberService.java
+++ src/main/java/com/shop/service/MemberService.java
@@ -11,6 +11,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.List;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -45,5 +47,29 @@ public class MemberService implements UserDetailsService {
                 .roles(member.getRole().toString())
                 .build();
     }
+
+    public List<Member> findAllMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findByEmail(String email) {
+        return memberRepository.findByEmail(email);
+    }
+
+    public void updateMember(Member member) {
+        Member existingMember = memberRepository.findById(member.getId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 회원을 찾을 수 없습니다."));
+        existingMember.setName(member.getName());
+        existingMember.setEmail(member.getEmail());
+        existingMember.setAddress(member.getAddress());
+        // 필요한 다른 수정 작업을 수행할 수 있습니다.
+        memberRepository.save(existingMember);
+    }
+
+    public Member findById(Long id) {
+        return memberRepository.findById(id)
+                .orElse(null); // 회원이 없을 경우 null을 반환하거나 예외 처리를 수행하세요.
+    }
+
 
 }
